678f1340ae140f402ec9c0210e21478f4ccec801
Anton Eliseev <anton_saintp@mail.ru>
2023-11-24 16:38:47 +0300

    demo input

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { copy, linkIcon, loader, tick } from "../assets";
 
 const Demo = () => {
-  return (
-    <div>Demo</div>
-  )
-}
+  const [article, setArticle] = useState({
+    url: "",
+    summary: "",
+  });
 
-export default Demo
+  const handleSubmit = () => {
+   
+  };
+
+  return (
+    <>
+      <section className="mt-16 w-full max-w-xl">
+        {/* Search */}
+        <div className="flex flex-col w-full gap-2">
+          <form
+            className="relative flex justify-center items-center"
+            onSubmit={handleSubmit}
+          >
+            <img
+              src={linkIcon}
+              alt="link_icon"
+              className="absolute left-0 my-2 ml-3 w-5"
+            />
+
+            <input
+              type="url"
+              className="url_input peer"
+              placeholder="Enter a URL"
+              value={article.url}
+              onChange={(e) => {
+                setArticle({
+                  ...article,
+                  url: e.target.value,
+                });
+              }}
+              required
+            />
+
+            <button
+              type="submit"
+              className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700"
+            >
+              <p>↵</p>
+            </button>
+          </form>
+
+          {/* Browse URL History */}
+        </div>
+        {/* Display results */}
+      </section>
+    </>
+  );
+};
+
+export default Demo;
